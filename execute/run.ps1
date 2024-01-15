268b9705b81c2db666109b4fb613320e42b2a1cd
Karl Stoerzel <karl-erik.stoerzel@epitech.eu>
2024-01-15 09:12:55 +0100

    run.ps1 returns exitcode of script

--- execute/run.ps1
+++ execute/run.ps1
@@ -7,4 +7,13 @@ $pythonScript = "DDA-LGB-ML.py"
 $pythonScriptPath = Join-Path $scriptDirectory $pythonScript
 
 # Execute the Python script
-python $pythonScriptPath
+$process = Start-Process -FilePath "python" -ArgumentList $pythonScriptPath -PassThru -Wait
+
+# Check the exit code of the Python script
+if ($process.ExitCode -eq 0) {
+    Write-Host "Python script completed successfully. Exit code: $($process.ExitCode)"
+    exit 0
+} else {
+    Write-Host "Python script encountered an error. Exit code: $($process.ExitCode)"
+    exit $process.ExitCode
+}
